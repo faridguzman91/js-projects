f8ee7481e1d6122e877d5f8de96fdb2bff5cb95b
Jimmy Guzman <jimmy@creapolis.com>
2021-03-25 21:59:23 +0100

    project03

--- js/js-02/main02.js
+++ js/js-02/main02.js
@@ -3,40 +3,41 @@ const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('circle') //create array of all circle items
 
-let currentActive = 1; //start counter at 1 by default
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('active')
+    progress.style.width = (actives.length / circles.length) * 100 + '%'
+
+}
+
+let currentActive = 1 //start counter at 1 by default
 
 next.addEventListener('click', () => {
     //adds 1 to the counter on the current circle
-    currentActive++;
+    currentActive++
 
     //Allow the counter not pass the number of circles in array
 
     if (currentActive > circles.length) {
-        currentActive = circles.length;
+        currentActive = circles.length
     }
-    update();
-});
+    update()
+})
 
 prev.addEventListener('click', () => {
     //decrement 1 to the counter on the current circle
     currentActive--;
     //if its not bigger than the amount of circles return to 1 instead of 0 and -1
     if (currentActive < 1) {
-        currentActive = 1;
+        currentActive = 1
     }
-    update();
-});
-
-function update() {
-    circles.forEach((circle, idx) => {
-        if (idx < currentActive) {
-            circle.classList.add('active');
-        } else {
-            circle.classList.remove('active');
-        }
-    });
-
-    const actives = document.querySelectorAll('active');
-    progress.style.width = (actives.length / circles.length) * 100 + '%';
-
-};
+    update()
+})
